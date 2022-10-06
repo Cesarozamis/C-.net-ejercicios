89e5dc8ebef95b48e708a6ae06b8c84913b4f5a6
Cesar Ozamis <cesarozamis@hotmail.com>
2022-10-06 11:59:03 -0300

    t

---
+++ operadores/Consoleapp-operadores/Consoleapp-operadores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleapp_operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 != 6);
+            
+            Console.WriteLine(7 == 8);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            
+        }
+    }
+}
