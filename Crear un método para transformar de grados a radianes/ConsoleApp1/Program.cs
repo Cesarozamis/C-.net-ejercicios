75a40a420f30217252544c165dfe21be6c5c423b
Cesar Ozamis <cesarozamis@hotmail.com>
2022-10-19 12:51:32 -0300

    work

---
+++ Crear un método para transformar de grados a radianes/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double gradosAr, radianes;
+
+            Console.Write("Ingresa los grados: ");
+            gradosAr = Convert.ToDouble(Console.ReadLine());
+
+            radianes = GradosRadianes(gradosAr);
+
+            Console.WriteLine("{0}° = {1} radianes", gradosAr, radianes);
+
+        }
+
+        static double GradosRadianes(double gradosPa)
+        {
+            double radianes;
+
+            radianes = (gradosPa * Math.PI) / 180;
+
+            return radianes;
+        }
+    }
+}
